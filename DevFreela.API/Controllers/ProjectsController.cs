598a3023f1f121797cba796fcd9c490e5ac3a61a
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-08-20 16:30:27 -0300

    Basic structure of the project.

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,76 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        // api/projects?query=net core
+       [HttpGet]
+       public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+
+        // api/projects/2
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            // return NotFound();
+
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if(createProject.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id}, createProject);
+        }
+
+        // api/projects/2
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if(updateProject.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Se não existir 404
+
+            // Se existir
+            return NoContent();
+        }
+
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, [FromBody] CreateCommentModel createComment)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            return NoContent();
+        }
+    }
+}

95c3d09e373e633595792b668f158db4aba853dd
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-08-22 17:24:14 -0300

    01 - Core

---
+++ DevFreela.Core/Entities/ProjectComment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DevFreela.Core.Entities
+{
+    public class ProjectComment : BaseEntity
+    {
+        public ProjectComment(string content, int idProject, int idUser)
+        {
+            Content = content;
+            this.idProject = idProject;
+            this.idUser = idUser;
+        }
+
+        public string Content { get; private set; }
+        public int idProject { get; private set; }
+        public int idUser { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+    }
+}

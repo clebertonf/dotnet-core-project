6670e6fb691237adcc5da50268bccda224fbacb9
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-08-30 22:11:02 -0300

    add dapper version

--- DevFreela.Application/Services/Implementations/SkillService.cs
+++ DevFreela.Application/Services/Implementations/SkillService.cs
@@ -1,6 +1,9 @@
-﻿using DevFreela.Application.Services.Interfaces;
+﻿using Dapper;
+using DevFreela.Application.Services.Interfaces;
 using DevFreela.Application.ViewModels;
 using DevFreela.infrastructure.Persistence;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,18 +11,30 @@ namespace DevFreela.Application.Services.Implementations
 {
     public class SkillService : ISkillService
     {
+        private readonly string _connectionString;
         private readonly DevFreelaDbContext _dbContext;
-        public SkillService(DevFreelaDbContext dbContext)
+        public SkillService(DevFreelaDbContext dbContext, IConfiguration configuration)
         {
             _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
         }
 
         public List<SkillViewModel> GetAll()
         {
-            var skills = _dbContext.Skills;
-            var skillsViewModel = skills.Select(s => new SkillViewModel(s.Id, s.Description)).ToList();
+            /* var skills = _dbContext.Skills;
+               var skillsViewModel = skills.Select(s => new SkillViewModel(s.Id, s.Description)).ToList();
 
-            return skillsViewModel;
+               return skillsViewModel;
+            */
+
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+
+                var query = "SELECT Id, Description FROM Skills";
+
+                return sqlConnection.Query<SkillViewModel>(query).ToList();
+            }
         }
     }
 }
